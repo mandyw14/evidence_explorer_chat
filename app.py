00c4a9482b7522e8b491faeb3a74928d9079a46a
Mandy Wintink [They/She] <82188550+mandyw14@users.noreply.github.com>
2026-03-06 10:32:43 -0500

    Modify default query for PubMed search
    Updated the default query to include mindfulness and additional filters.

--- app.py
+++ app.py
@@ -164,10 +164,16 @@ def efetch_details(pmids: list[str]) -> pd.DataFrame:
 # -----------------------------
 st.sidebar.header("Search settings")
 
-default_query = (
+#default_query = (
+ #   '("multiple sclerosis"[Title/Abstract] OR "multiple sclerosis"[MeSH Terms]) '
+ #   'AND ("exercise"[Title/Abstract] OR "exercise"[MeSH Terms] OR "physical activity"[Title/Abstract])'
+#)
+#default_query = (
     '("multiple sclerosis"[Title/Abstract] OR "multiple sclerosis"[MeSH Terms]) '
-    'AND ("exercise"[Title/Abstract] OR "exercise"[MeSH Terms] OR "physical activity"[Title/Abstract])'
-)
+    AND ("exercise"[MeSH Terms] OR "mindfulness"[MeSH Terms]) 
+    AND ((y_10[Filter]) AND (clinicalstudy[Filter] OR clinicaltrial[Filter] 
+    OR meta-analysis[Filter] OR randomizedcontrolledtrial[Filter] OR review[Filter] OR scopingreview[Filter] OR systematicreview[Filter] OR validationstudy[Filter]) AND (humans[Filter]) AND (english[Filter])
+ )
 
 query = st.text_input(
     "Search PubMed",
