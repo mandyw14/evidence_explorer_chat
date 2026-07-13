97bef645b70cb3eed87e518191f6b801e09f51b8
Mandy Wintink [They/She] <82188550+mandyw14@users.noreply.github.com>
2026-07-13 12:34:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -574,6 +574,7 @@ st.subheader("Search setup")
 
 selected_condition = st.selectbox(
     "Step 1: Choose a neurological condition",
+    "this is how you choose a neurological condition."
     options=COMMON_CONDITIONS,
     index=0,
     help=(
