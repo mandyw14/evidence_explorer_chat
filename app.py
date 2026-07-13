c794c80e1047568d30f37b32a165b6e30d0405a8
Mandy Wintink [They/She] <82188550+mandyw14@users.noreply.github.com>
2026-07-13 12:34:16 -0400

    Remove placeholder text in condition selection
    Remove placeholder text for neurological condition selection.

--- app.py
+++ app.py
@@ -574,7 +574,6 @@ st.subheader("Search setup")
 
 selected_condition = st.selectbox(
     "Step 1: Choose a neurological condition",
-    "this is how you choose a neurological condition."
     options=COMMON_CONDITIONS,
     index=0,
     help=(
