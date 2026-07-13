cbe14c0be7df776f3fc097b88dc8b3973a08a462
Mandy Wintink [They/She] <82188550+mandyw14@users.noreply.github.com>
2026-07-13 12:10:47 -0400

    Enhance intervention categories and descriptions
    Expanded intervention categories and descriptions with additional terms and examples.

--- dictionaries.py
+++ dictionaries.py
@@ -12,13 +12,14 @@ COMMON_CONDITIONS = [
 
 INTERVENTION_CATEGORIES = {
 
-   "General Complementary and Alternative Medicine": ["complementary medicine", "alternative medicine", "Holistic", "Integrative Medicine"],
+   "General Complementary and Alternative Medicine": ["complementary medicine","complementary therapy","alternative medicine", "alternative therapy", "Holistic Health", "Integrative Medicine"],
     
     "Mind-body": [
         "mindfulness",
         "meditation",
         "yoga",
         "tai chi",
+        "qigong",
     ],
 
     "Exercise": [
@@ -29,15 +30,20 @@ INTERVENTION_CATEGORIES = {
     ],
   
     "Diets & Nutrition": [
+        "diet",
         "nutrition",
         "Mediterranean",
         "Paleo",
         "MIND",
         "Ketogenic",
+        "nutrition",
         "vegetarian",
         "vegan",
+        "antioxidant",
+        "high-protein diet",
         "low-fat diet",
         "low-carbohydrate diet",
+        "dietician",
     ],
             
     "Supplements & Nutraceuticals": [
@@ -46,11 +52,52 @@ INTERVENTION_CATEGORIES = {
         "probiotics",
         "omega-3",
         "vitamin D",
+        "microbiome",
+        "fecal transplants",
+        "microbiome treatments",
         "prebiotics",
+        "byturic acid",
         "fish oils",
+        "hyperbaric oxygen",
         "antioxidants",
         "magnesium"
-    ], 
+    ],
+
+    "Gut Health": [
+        "microbiome",
+        "fecal microbiota transplants",
+        "microbiota",
+        "gut health",
+        "probiotic",
+    ],
+    
+
+    "Neurotechnology": [
+        "transcranial magnetic stimulation",
+        "transcranial direct current stimulation",
+        "intermittent theta burst stimulation",
+        "focused ultrasound",
+        "neurofeedback",
+        "biofeedback",
+        "deep brain stimulation",
+        "vagus nerve stimulation",
+        "red light therapy",
+        "electroceutical",
+        "spinal cord stimulation",
+    ],
+
+    "Neurostimulation": [
+        "Transcranial magnetic stimulation", "Transcranial Direct Current Stimulation", 
+        "Intermittent theta-burst stimulation", 
+         "ultrasound",  "Focused Ultrasound", "transcranial focused ultrasound", 
+        "Light Flickering Stimulation",   "Epidural Electrical Stimulation",  "electrical stimulation",      
+        "Frequency-Domain Near-Infrared Spectroscopy", "fNIRS", 
+        "spinal cord stimulation",
+        "deep brain stimulation",
+        "vagus nerve stimulation",
+    ]
+
+    
 }
 
 
@@ -98,6 +145,30 @@ cellular energy, or other biological pathways.
 There are many types of exercise that exist. These approaches may include aerobic exercise (like a cardio workout), resistance (like lifting weights), or special programs like dance or pilates, 
 and sometimes practices like yoga or Tai Chi.
 """,
+        "examples": [
+            "resistance training",
+            "aerobic exercise",
+            "weight training",
+            "running",
+        ],
+        "video": ""
+    },
+
+    "Neurotechnology": {
+        "description": """
+Neurotechnology involves a variety of technologies that stimulate the brain, both invasively and non-invasively. 
+It also includes neurofeedback technology. Generally, this also includes the term "neuromodulation". 
+""",
+        "examples": [
+            "Transcranial magnetic stimulation",
+            "vagus nerve stimulation",
+            "neurofeedback",
+            "functional electrical stimulation",
+            "ultrasound",
+        ],
+        "video": ""
+    },
+
         "Diet & Nutrition": {
         "description": """
 There are so many different kids of diets that exist, including, vegan, vegetarian, paleo diets, ketogenic diets, the MIND diet, 
